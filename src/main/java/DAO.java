e030ba68e84ce8bc6e9b63ce15c6d43453c9ede4
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2022-06-05 18:33:35 -0300

    Segundo Commit

--- src/main/java/DAO.java
+++ src/main/java/DAO.java
@@ -8,6 +8,7 @@ public class DAO {
 
     public boolean existe(Usuario usuario) throws Exception {
         String sql = "SELECT * FROM tb_usuario WHERE nome = ? AND senha= ?";
+        
  try ( Connection conn = ConexaoBD.obterConexao();  PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, usuario.getNome());
             ps.setString(2, usuario.getSenha());
